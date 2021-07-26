1387f7e280d8ef10131762972ad3d1c1dad04491
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-07-26 20:16:22 +0300

    массив + проверка на корректность вывода по индексу

--- Lesson2/ViewController.swift
+++ Lesson2/ViewController.swift
@@ -9,17 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let array: [String] = ["123", "qswe"]
+    var array: [String] = ["123", "qswe"]
     let arrayOne: [String] = Array()  //пустой массив
     let arrayOne1: [String] = Array<String>(["asda", "asd"])  //еще один способ
     let arrrayOne2: [String] = Array(arrayLiteral: "asf", "awe")
     let array4 = [String] ()  // одна из разновидностей
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     // Do any additional setup after loading the view.
         printHelloWorld()
         printName(name: "Vlad", "Sergei")
+        array.append("d")  //добавление в массив
+        array.insert("zzzz", at: 1) // добавление zzzz на 1  позицию  массива
+        
+        print (array)
+        print (array[1]) //элемент массива с индексом 0
+        
+        
+        
+    }
+    
+    func printElementOfArray(array: [String], index: Int) {
+        if index >= 0 && index < array.count {
+            print (array[index])
+        } else {
+            print ("invalid index")
+        }
+         
     }
     
     func printHelloWorld() {
